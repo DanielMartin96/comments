95fa9339d94be88838eaf02069ff9010375bd3a0
Daniel Martin <daniel.martin@rentokil-initial.com>
2022-03-20 19:57:26 +0000

    changed name of database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,19 +4,19 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/DanielMartin96/comments/internal/db"
+	"github.com/DanielMartin96/comments/internal/database"
 )
 
 // Run - going to responsible for instantiation and start up of our application
 func Run() error {
 	fmt.Println("Starting our application")
 
-	db, err := db.NewDatabase()
+	database, err := database.NewDatabase()
 	if err != nil {
 		fmt.Println("failed to connect to database")
 		return err
 	}
-	if err := db.Ping(context.Background()); err != nil {
+	if err := database.Ping(context.Background()); err != nil {
 		return err
 	}
 	fmt.Println("successfully pinged database")
