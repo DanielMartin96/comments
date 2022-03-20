c64342ac70c4a4b41358c07138a06f53a959fb62
Daniel Martin <daniel.martin@rentokil-initial.com>
2022-03-20 19:52:17 +0000

    connected to database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/DanielMartin96/comments/internal/db"
+)
 
 // Run - going to responsible for instantiation and start up of our application
 func Run() error {
 	fmt.Println("Starting our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("failed to connect to database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("successfully pinged database")
 	return nil
 }
 
