77e9d3455f853a8de7f565dc119b68f06aafefb0
Daniel Martin <daniel.martin@rentokil-initial.com>
2022-03-20 17:32:31 +0000

    Added the initial entrypoint

--- cmd/internal/main.go
+++ cmd/internal/main.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+// Run - going to responsible for instantiation and start up of our application
+func Run() error {
+	fmt.Println("Starting our application")
+	return nil
+}
+
+func main(){
+	fmt.Println("Go REST API Course")
+	if err := Run(); err != nil {
+		fmt.Println(err)
+	}
+}
